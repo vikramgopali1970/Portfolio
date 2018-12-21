37522d99109f9b6ad71891fb6ad96aadcca15bb3
Vikram Gopali <vikramgopali.93@gmail.com>
2018-12-21 02:21:55 -0600

    Contact us

--- src/Components/ContactMe/ContactMe.js
+++ src/Components/ContactMe/ContactMe.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import './ContactMe.css';
+
 
 const ContactMe=()=>{
     return (
-        <div>
-            this is contact me.
-        </div>
+                <div className="contact-me">
+                    <div className="info">
+                        <h2>Contact Me</h2>
+                        <address className="paragrph">
+                            800 W Renner Rd, #2926<br/>
+                            Richardson, TX, 75080 <br/>
+                            United States<br/>
+                        </address>
+                        <p className="paragrph">
+                            <strong>Email:</strong> me@vikramgopali.com <br/>
+                            <strong>Phone:</strong> +1 (469)-380-4228
+                        </p>
+                    </div>
+                    <div className="cardEmailMMe">
+                        <form className="form">
+                            <label>Name *</label>
+                            <input type="text" name="Name" placeholder="Your Name"/>
+                            <label>Email Adress *</label>
+                            <input type="email" name="Email Adress" placeholder="Your E-mail "/>
+                            <label>Message *</label>
+                            <textarea name="Message" rows="10" cols="80" placeholder="Your Message"></textarea>
+                            <button className="contactButton">Send Mail</button>
+                        </form>
+                    </div>
+                </div>
     );
 };
 
--- src/Components/Quote/Quote.js
+++ src/Components/Quote/Quote.js
@@ -2,22 +2,30 @@ import React from 'react';
 import './Quote.css';
 
 
-const quote = [
-    {
-    quote : 'There is No Substitute for Hard Work.',
-    author : 'Thomas A Edison'
-    },
-    {
-        quote : 'Failure is the opportunity to begin again, more intelligently.',
-        author : 'Henry Ford'
-    }
-];
 
-const Quote = ()=>(
-    <div className="quote">
-        <label>{quote[0].quote}</label><br/>
-        <label>- {quote[0].author}</label>
-    </div>
-);
+
+const Quote = ()=>{
+    let quote = [
+        {
+            quote : 'There is No Substitute for Hard Work.',
+            author : 'Thomas A Edison'
+        },
+        {
+            quote : 'Failure is the opportunity to begin again, more intelligently.',
+            author : 'Henry Ford'
+        }
+    ];
+
+    let ind = Math.floor(Math.random()*(quote.length))
+    console.log();
+    return(
+        <div className="blockQuote">
+            <div className="quoteSymbol"></div>
+            <div className="quote">{quote[ind].quote}</div><br/>
+            <div className=" quote quoteAuthor">- {quote[ind].author}</div>
+        </div>
+    );
+}
+
 
 export default Quote;
